b1eaba1f8beb213e223a4eebc45627b5f76b9125
krak_os <48405896+missedpoints@users.noreply.github.com>
2023-01-18 14:45:38 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 //This program was written by Yazeed M. Hakami, for BE1610.
-use std::num; // random library call, really didn't need this in the end but it was good having it!
 use std::f64;
 use std::io;
-const euler: f64 = std::f64::consts::E; // Euler's number, "e"
+const EULER: f64 = std::f64::consts::E; // Euler's number, "e"
 
 fn main() {
 
@@ -10,6 +9,9 @@ fn main() {
     let resistence: f64 = 100000.0; // Luv me resistors, simple as. (this is in Ohms.)
 
     println!("Please input your desired voltage in decimal form.");
+    println!("WARNING! --- If you wish to recieve the same results as the paper, please input the following values:");
+    println!("5, 50.");
+
 
     let mut voltage = String::new();
     io::stdin().read_line(&mut voltage).unwrap();
@@ -21,15 +23,15 @@ fn main() {
     io::stdin().read_line(&mut step).unwrap();
     let step: f64 = step.trim().parse().unwrap();
 
-    for i in (0..=(voltage*(10 as f64)) as i64).step_by(step as usize) {
-        let Ic: f64 = (voltage)/resistence * euler.powf(-i as f64/(resistence*capacitence));
-        println!("Discharge capacitance at point {} is {}",i,Ic*10.0_f64.powf(5 as f64).abs()); // Multiplied by 10^5 so I don't get some teensy tiny number, my eyes are fragile!
+    for i in (0..=(step*(10 as f64)) as i64).step_by(step as usize) {
+        let ic: f64 = (voltage)/resistence * EULER.powf(-i as f64/(resistence*capacitence));
+        println!("Discharge capacitance at point {} is {}",i,ic*10.0_f64.powf(5 as f64).abs()); // Multiplied by 10^5 so I don't get some teensy tiny number, my eyes are fragile!
         //Should also add that because I'm multiplying by 10^5, actual results are 10^-5
     }
 
-    for i in (0..=(voltage*(10 as f64)) as i64).step_by(step as usize) {
-        let Vc: f64 = voltage*(1.0 - euler.powf(-i as f64/(resistence*capacitence)));
-        println!("Charge capacitance at point {} is {}",i,Vc.abs());
+    for i in (0..=(step*(10 as f64)) as i64).step_by(step as usize) {
+        let vc: f64 = voltage*(1.0 - EULER.powf(-i as f64/(resistence*capacitence)));
+        println!("Charge capacitance at point {} is {}",i,vc.abs());
     }
     //Decided to do this in code instead of by hand because of my extensive background in programming, and because it was significantly easier than doing the same operation 10 times ;)
     
